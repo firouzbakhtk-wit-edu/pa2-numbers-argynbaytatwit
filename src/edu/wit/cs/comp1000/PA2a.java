dc4014b91209a9e45208d50e942f0b1063a00fdb
Temirlan Argynbay <argynbayt@wit.edu>
2024-10-03 11:14:28 -0400

    PA2 by Temirlan Argynbay.

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,32 @@
 package edu.wit.cs.comp1000;
-
-// TODO: document this class
+import java.util.Scanner;
+// TODO: Get 5 integers and output 6 things. 
 public class PA2a {
 
-	// TODO: document this function
+	// TODO: I thought about easier way of inputing here. 
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter 5 integers: ");
+		int sum_pos=0, sum_neg=0;
+		int inputs;
+		double number_pos = 0.00, number_neg=0.00;
+		for(int i = 0; i<5; i++) {
+			inputs = input.nextInt();
+			if (inputs > 0) {
+				sum_pos += inputs;
+				number_pos +=1 ;
+			}
+			else {
+				sum_neg += inputs;
+				number_neg +=1;
+			}
+		}
+		System.out.println("The sum of all of the positive numbers: " + sum_pos);
+		System.out.println("The sum of all of the non-positive numbers: " + sum_neg);
+		System.out.println("The sum of all five numbers: " + (sum_pos + sum_neg));
+		System.out.println("The average of all of the positive numbers: " + sum_pos/number_pos);
+		System.out.println("The average of all of the non-positive numbers: " + sum_neg/number_neg);
+		System.out.println("The average of all five numbers: " + (sum_pos + sum_neg)/5.00);
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,29 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
-// TODO: document this class
+// TODO: Part B: getting coefficients for quadratic equation. Getting out the roots.
 public class PA2b {
 
-	// TODO: document this function
+	// TODO: define the a, b, and c, calculate the Discriminant, and get out the roots as outputs.
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter a, b, c: ");
+		a = input.nextInt();
+		b = input.nextInt();
+		c = input.nextInt();
+		int d = b*b - 4*a*c;
+		double r1= (-b - Math.sqrt(d))/(2*a);
+		double r2= (-b + Math.sqrt(d))/(2*a);
+		if (d < 0) {
+			System.out.println("Roots: imaginary");
+		}
+		else if(d ==0){
+			System.out.println("Root = " + (double)(-b/(2*a)));
+		}
+		else {
+			System.out.println("Roots :" + Double.toString(r1) + " and "+ Double.toString(r2));
+		}
 	}
 
 }
